29d2e248915cec54af1f61a982fc52e249827e9e
mylasousa12 <90429473+mylasousa12@users.noreply.github.com>
2026-04-16 22:51:56 -0300

    WIP

--- src/api/src/app/api/services/weather/route.ts
+++ src/api/src/app/api/services/weather/route.ts
@@ -1,22 +1,12 @@
-import {NextRequest, NextResponse} from "next/server";
+import { NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
-    const {searchParams} = new URL(req.url);
+export async function GET() {
 
-    const lat = searchParams.get("lat");
-    const lon = searchParams.get("lon");
-
-    if (!lat || !lon) {
-        return NextResponse.json(
-            {error: "Latitute e longitude são obrigatorias"},
-            {status: 400}
-        )
-    }
-
-    const apiKey = process.env.OPENWEATHER_API_KEY;
+    const apiKey = process.env.API_KEY;
+    const baseUrl = process.env.BASE_URL;
 
     try {
-        const response = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}`);
+        const response = await fetch(`${baseUrl}/current.json?key=${apiKey}&q=auto:ip`);
         if (!response.ok) {
             throw new Error("Erro ao buscar a api");
         }
